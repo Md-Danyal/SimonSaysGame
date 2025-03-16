aac811cb1d6ceb1df72b06e5c0e8459b9ddd8a38
Md-Danyal <111244372+Md-Danyal@users.noreply.github.com>
2025-03-16 15:37:47 +0530

    Update app.js
    added dark theme

--- app.js
+++ app.js
@@ -94,3 +94,16 @@ function reset() {
     gameSeq = [];
     level = 0;
 }
+
+// theme toggling
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function() {
+    document.body.classList.toggle("dark");
+    if(document.body.classList.contains("dark")) {
+        icon.src = "assets/sun.png";
+    } else {
+        icon.src = "assets/moon.png";
+    }
+}
